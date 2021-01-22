12980d506be3f14415511f81d7dd5abf63cb956b
alextran0713 <quyen137@csu.fullerton.edu>
2021-01-22 01:38:28 -0800

    add model part

---
+++ backend/models/Part.js
@@ -0,0 +1,25 @@
+const { Sequelize, Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Part extends Model {}
+    Part.init({
+        id:{
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        model: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+      }, {    
+        sequelize,
+        tableName: 'parts',
+        modelName: 'Parts',
+    });
+    return Part
+}
